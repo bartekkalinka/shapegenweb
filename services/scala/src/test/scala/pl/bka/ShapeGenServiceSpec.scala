a90e1667a1998fc0347d48c403e81b8a97d82e38
Bartek Kalinka <bartkali@hotmail.com>
2015-02-15 18:18:35 +0100

    tests back to working

--- services/scala/src/test/scala/pl/bka/ShapeGenServiceSpec.scala
+++ services/scala/src/test/scala/pl/bka/ShapeGenServiceSpec.scala
@@ -1,29 +1,29 @@
-package com.example
+package pl.bka
 
 import org.specs2.mutable.Specification
 import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
 
-class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
+class ShapeGenServiceSpec extends Specification with Specs2RouteTest with ShapeGenService {
   def actorRefFactory = system
   
   "MyService" should {
 
     "return a greeting for GET requests to the root path" in {
-      Get() ~> myRoute ~> check {
+      Get() ~> mainRoute ~> check {
         responseAs[String] must contain("Say hello")
       }
     }
 
     "leave GET requests to other paths unhandled" in {
-      Get("/kermit") ~> myRoute ~> check {
+      Get("/kermit") ~> mainRoute ~> check {
         handled must beFalse
       }
     }
 
     "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> sealRoute(myRoute) ~> check {
+      Put() ~> sealRoute(mainRoute) ~> check {
         status === MethodNotAllowed
         responseAs[String] === "HTTP method not allowed, supported methods: GET"
       }
