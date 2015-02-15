859c5b0d0fc8aceb6bf550528b7074a0a8bf696b
Bartek Kalinka <bartkali@hotmail.com>
2015-02-15 18:57:52 +0100

    serving static html file

--- services/scala/src/main/scala/pl/bka/Service.scala
+++ services/scala/src/main/scala/pl/bka/Service.scala
@@ -24,17 +24,7 @@ class ShapeGenServiceActor extends Actor with ShapeGenService {
 trait ShapeGenService extends HttpService {
 
   val mainRoute =
-    path("") {
-      get {
-        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
-          complete {
-            <html>
-              <body>
-                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
-              </body>
-            </html>
-          }
-        }
-      }
+    pathPrefix("") {
+      getFromResourceDirectory("")
     }
 }
