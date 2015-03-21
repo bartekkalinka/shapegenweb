34b424be06651b9f952855f8e4994d31a33d2a3e
Bartek Kalinka <bartkali@hotmail.com>
2015-03-21 23:36:05 +0100

    some Core tests

--- scala/src/test/scala/pl/bka/shapegenweb/CoreSpec.scala
+++ scala/src/test/scala/pl/bka/shapegenweb/CoreSpec.scala
@@ -6,5 +6,15 @@ package pl.bka.shapegenweb
 import org.scalatest.{Matchers, FlatSpec}
 
 class CoreSpec extends FlatSpec with Matchers {
-  //TODO
+  "Terrain.get" should "cache results" in {
+    val noise1 = Terrain.get(49, 51, 0)
+    val noise2 = Terrain.get(49, 51, 0)
+    noise1 should be (noise2)
+  }
+
+  "Terrain.get" should "size noise based on detail parameter" in {
+    val noise1 = Terrain.get(49, 51, 0)
+    val noise2 = Terrain.get(49, 51, 1)
+    noise1.noise.length should be (noise2.noise.length / 2)
+  }
 }
